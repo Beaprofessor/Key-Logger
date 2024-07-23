20116ee82f01553bedabe30854038fd241a43092
Beaprofessor <professornberlin2122@gmail.com>
2024-07-23 16:21:13 +0530

    Key Logger -> Done
    Where you can track and monitor key presses in real time

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/index.html",
+    "./dist/script.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
